448a9bbff592e684c2657c26895358ec3da9293d
Oluwatobiloba Ogunye <ogunye4nao@gmail.com>
2019-07-31 21:58:53 +0100

    -v 0.1.2 : Fixed problem with json assets not loading

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
 ## [0.0.1] - TODO: Add release date.
 
 * TODO: Describe initial release.
+
+## [0.0.2] - TODO: Add release date.
+
+* TODO: Describe initial release.
+
+## [0.0.3] - TODO: Add release date.
+
+* TODO: Describe initial release.
--- lib/providers/country_provider.dart
+++ lib/providers/country_provider.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
 
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/widgets.dart';
 import 'package:intl_phone_number_input/models/country_model.dart';
 
 class CountryProvider {
   Future<List<Country>> getCountriesDataFromJsonFile(
       {@required BuildContext context}) async {
-    var list = await DefaultAssetBundle.of(context)
-        .loadString('assets/countries.json');
+    var list = await rootBundle.loadString('assets/countries.json');
     List jsonList = jsonDecode(list);
 
     return jsonList.map((country) => Country.fromJson(country)).toList();
