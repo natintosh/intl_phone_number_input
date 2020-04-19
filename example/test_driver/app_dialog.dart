af236b722a93b4e43d864d438403383d241b8b38
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2020-04-19 12:38:08 +0100

    Updates: Updated package example folder and added integration test

---
+++ example/test_driver/app_dialog.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:intlphonenumberinputtest/main_dialog.dart' as app;
+
+main() {
+  enableFlutterDriverExtension();
+
+  app.main();
+}
