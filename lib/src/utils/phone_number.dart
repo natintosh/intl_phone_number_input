683a7a730cfb982351b2f6031d8b7302c50a71d1
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2020-05-19 09:42:51 +0100

    updated: PhoneNumber object

--- lib/src/utils/phone_number.dart
+++ lib/src/utils/phone_number.dart
@@ -64,7 +64,7 @@ class PhoneNumber extends Equatable {
         .replaceAll(RegExp('^([\\+?${this.dialCode}\\s?]+)'), '');
   }
 
-  ///For predefined phone number - get the initial country ISO2 code from the dial code for you to pass as the [initialCountry2LetterCode]
+  ///For predefined phone number - get the initial country ISO2 code from the dial code 
   static String getISO2CodeByPrefix(String prefix) {
     if (prefix != null && prefix.isNotEmpty) {
       prefix = prefix.startsWith('+') ? prefix : '+$prefix';
