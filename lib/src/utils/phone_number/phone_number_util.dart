7050538bbee00c64d0917aa3d43a57b4ebdfe21a
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2021-03-31 15:02:56 +0100

    Fixed issue with region prefix returning null

--- lib/src/utils/phone_number/phone_number_util.dart
+++ lib/src/utils/phone_number/phone_number_util.dart
@@ -69,14 +69,14 @@ class RegionInfo {
   RegionInfo({this.regionPrefix, this.isoCode, this.formattedPhoneNumber});
 
   RegionInfo.fromJson(Map<String, dynamic> json) {
-    regionPrefix = json['regionPrefix'];
+    regionPrefix = json['regionCode'];
     isoCode = json['isoCode'];
     formattedPhoneNumber = json['formattedPhoneNumber'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['regionPrefix'] = this.regionPrefix;
+    data['regionCode'] = this.regionPrefix;
     data['isoCode'] = this.isoCode;
     data['formattedPhoneNumber'] = this.formattedPhoneNumber;
     return data;
