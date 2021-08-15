66507ab1dff77d01a5d63294dde40e58135dc15f
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2021-08-15 17:50:49 +0100

    Added RTL Directionality support to Bottom Sheet and Dialog

--- lib/src/widgets/selector_button.dart
+++ lib/src/widgets/selector_button.dart
@@ -118,20 +118,23 @@ class SelectorButton extends StatelessWidget {
 
   /// shows a Dialog with list [countries] if the [PhoneInputSelectorType.DIALOG] is selected
   Future<Country?> showCountrySelectorDialog(
-      BuildContext context, List<Country> countries) {
+      BuildContext inheritedContext, List<Country> countries) {
     return showDialog(
-      context: context,
+      context: inheritedContext,
       barrierDismissible: true,
       builder: (BuildContext context) => AlertDialog(
-        content: Container(
-          width: double.maxFinite,
-          child: CountrySearchListWidget(
-            countries,
-            locale,
-            searchBoxDecoration: searchBoxDecoration,
-            showFlags: selectorConfig.showFlags,
-            useEmoji: selectorConfig.useEmoji,
-            autoFocus: autoFocusSearchField,
+        content: Directionality(
+          textDirection: Directionality.of(inheritedContext),
+          child: Container(
+            width: double.maxFinite,
+            child: CountrySearchListWidget(
+              countries,
+              locale,
+              searchBoxDecoration: searchBoxDecoration,
+              showFlags: selectorConfig.showFlags,
+              useEmoji: selectorConfig.useEmoji,
+              autoFocus: autoFocusSearchField,
+            ),
           ),
         ),
       ),
@@ -140,9 +143,9 @@ class SelectorButton extends StatelessWidget {
 
   /// shows a Dialog with list [countries] if the [PhoneInputSelectorType.BOTTOM_SHEET] is selected
   Future<Country?> showCountrySelectorBottomSheet(
-      BuildContext context, List<Country> countries) {
+      BuildContext inheritedContext, List<Country> countries) {
     return showModalBottomSheet(
-      context: context,
+      context: inheritedContext,
       clipBehavior: Clip.hardEdge,
       isScrollControlled: isScrollControlled,
       backgroundColor: Colors.transparent,
@@ -156,24 +159,27 @@ class SelectorButton extends StatelessWidget {
           ),
           DraggableScrollableSheet(
             builder: (BuildContext context, ScrollController controller) {
-              return Container(
-                decoration: ShapeDecoration(
-                  color: Theme.of(context).canvasColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12),
+              return Directionality(
+                textDirection: Directionality.of(inheritedContext),
+                child: Container(
+                  decoration: ShapeDecoration(
+                    color: Theme.of(context).canvasColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
+                      ),
                     ),
                   ),
-                ),
-                child: CountrySearchListWidget(
-                  countries,
-                  locale,
-                  searchBoxDecoration: searchBoxDecoration,
-                  scrollController: controller,
-                  showFlags: selectorConfig.showFlags,
-                  useEmoji: selectorConfig.useEmoji,
-                  autoFocus: autoFocusSearchField,
+                  child: CountrySearchListWidget(
+                    countries,
+                    locale,
+                    searchBoxDecoration: searchBoxDecoration,
+                    scrollController: controller,
+                    showFlags: selectorConfig.showFlags,
+                    useEmoji: selectorConfig.useEmoji,
+                    autoFocus: autoFocusSearchField,
+                  ),
                 ),
               );
             },
