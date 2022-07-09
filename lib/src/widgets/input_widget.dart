7302425298dd1975235c01de11d95acdd1a0e190
Brendan <ejike.br@gmail.com>
2022-07-09 15:26:48 +0100

    Update input_widget.dart
    Fixed "Warning: Operand of null-aware operation '!' has type 'WidgetsBinding' which excludes null."

--- lib/src/widgets/input_widget.dart
+++ lib/src/widgets/input_widget.dart
@@ -320,7 +320,7 @@ class _InputWidgetState extends State<InternationalPhoneNumberInput> {
   String? validator(String? value) {
     bool isValid =
         this.isNotValid && (value!.isNotEmpty || widget.ignoreBlank == false);
-    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       if (isValid && widget.errorMessage != null) {
         setState(() {
           this.selectorButtonBottomPadding =
