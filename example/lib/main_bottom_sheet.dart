aecb3eb0bfa23ac67810b69b586b92c3ebe8c98c
Akintunde Segun <akintundeayofesegun@gmail.com>
2024-03-01 00:30:34 +0100

    update sdk version

--- example/lib/main_bottom_sheet.dart
+++ example/lib/main_bottom_sheet.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             ElevatedButton(
               onPressed: () {
-                formKey.currentState.validate();
+                formKey.currentState?.validate();
               },
               child: Text('Validate'),
             ),
@@ -78,13 +78,13 @@ class _MyHomePageState extends State<MyHomePage> {
     controller.text = parsableNumber;
 
     setState(() {
-      initialCountry = number.isoCode;
+      initialCountry = number.isoCode!;
     });
   }
 
   @override
   void dispose() {
-    controller?.dispose();
+    controller.dispose();
     super.dispose();
   }
 }
