a0721f09b31fa610440dc5baa337454659f34e9f
Karin Berg <karin@karinberg.de>
2026-06-28 19:14:39 +0200

    Wrapped ListTile inside Material(color: Colors.transparent, ...)
    This preserves ink splash and background behavior when the tile is rendered inside a decorated dialog/bottom sheet

--- lib/src/widgets/countries_search_list_widget.dart
+++ lib/src/widgets/countries_search_list_widget.dart
@@ -147,26 +147,29 @@ class DirectionalCountryListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      key: Key(TestHelper.countryItemKeyValue(country.alpha2Code)),
-      leading: (showFlags ? _Flag(country: country, useEmoji: useEmoji) : null),
-      title: Align(
-        alignment: AlignmentDirectional.centerStart,
-        child: Text(
-          '${Utils.getCountryName(country, locale)}',
-          textDirection: Directionality.of(context),
-          textAlign: TextAlign.start,
+    return Material(
+      color: Colors.transparent,
+      child: ListTile(
+        key: Key(TestHelper.countryItemKeyValue(country.alpha2Code)),
+        leading: (showFlags ? _Flag(country: country, useEmoji: useEmoji) : null),
+        title: Align(
+          alignment: AlignmentDirectional.centerStart,
+          child: Text(
+            '${Utils.getCountryName(country, locale)}',
+            textDirection: Directionality.of(context),
+            textAlign: TextAlign.start,
+          ),
         ),
-      ),
-      subtitle: Align(
-        alignment: AlignmentDirectional.centerStart,
-        child: Text(
-          '${country.dialCode ?? ''}',
-          textDirection: TextDirection.ltr,
-          textAlign: TextAlign.start,
+        subtitle: Align(
+          alignment: AlignmentDirectional.centerStart,
+          child: Text(
+            '${country.dialCode ?? ''}',
+            textDirection: TextDirection.ltr,
+            textAlign: TextAlign.start,
+          ),
         ),
+        onTap: () => Navigator.of(context).pop(country),
       ),
-      onTap: () => Navigator.of(context).pop(country),
     );
   }
 }
